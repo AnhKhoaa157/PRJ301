e2cec65525069d9c76e93cff3d0c873c09b1ba58
2imAnhKhoa <99414353+AnhKhoaa157@users.noreply.github.com>
2025-02-06 11:43:17 +0700

    update

--- WebApplication_06_a/src/java/test/UserTest.java
+++ WebApplication_06_a/src/java/test/UserTest.java
@@ -7,6 +7,7 @@ package test;
 
 import dao.UserDAO;
 import dto.UserDTO;
+import java.util.List;
 
 /**
  *
@@ -14,12 +15,20 @@ import dto.UserDTO;
  */
 public class UserTest {
 public static void main(String[] args) {
-        UserDAO userDAO = new UserDAO();
-//        UserDTO ud1 = new UserDTO("TLN01", "Le Nhat Tung", "AD", "khong_co_password");
-//        userDAO.create(ud1);
-        for (int i = 0; i < 10; i++) {
-            UserDTO ud_i = new UserDTO("USER"+i, "Nguyen Van "+i, "US", "_"+i);
-            userDAO.create(ud_i);
+    UserDAO userDAO = new UserDAO();
+////        UserDTO ud1 = new UserDTO("TLN01", "Le Nhat Tung", "AD", "khong_co_password");
+////        userDAO.create(ud1);
+//        for (int i = 0; i < 10; i++) {
+//            UserDTO ud_i = new UserDTO("USER"+i, "Nguyen Van "+i, "US", "_"+i);
+//            userDAO.create(ud_i);
+//        }
+//    }
+//    UserDTO ud2 = new UserDTO("TLN01", "Le Nhat Tung", "US", "Nothing");
+//    userDAO.update(ud2);
+//    userDAO.delete("TLN01");
+    List<UserDTO> l1 = userDAO.readAll();
+        for (UserDTO u : l1) {
+            System.out.println(u);
         }
     }
 }
