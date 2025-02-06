07c1cf476b170ed1b63bdb8469091652d67a0954
2imAnhKhoa <99414353+AnhKhoaa157@users.noreply.github.com>
2025-02-06 10:41:39 +0700

    update

---
+++ WebApplication_06_a/src/java/dto/UserDTO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*DTO (Data Transfer Object) */
+package dto;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class UserDTO {
+    
+}
